9d226afc82f1c82644b95209b43690a4f9bce03b
Davli4 <vladislauskirey@yandex.by>
2026-05-13 16:46:07 +0300

    feat: start service

---
+++ stats/stats-server/src/main/java/ru/practicum/stats/model/EndpointHit.java
@@ -0,0 +1,33 @@
+package ru.practicum.stats.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "hits")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EndpointHit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String app;
+
+    @Column(nullable = false)
+    private String uri;
+
+    @Column(nullable = false)
+    private String ip;
+
+    @Column(name = "hit_timestamp", nullable = false)
+    private LocalDateTime timestamp;
+}
