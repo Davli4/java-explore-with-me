42ba870cffda25993abfcfc3f70b1f5ba2b80b3f
Davli4 <vladislauskirey@yandex.by>
2026-05-24 17:06:07 +0300

    fix: 1

--- main-service/src/main/java/ru/practicum/ewm/EwmApplication.java
+++ main-service/src/main/java/ru/practicum/ewm/EwmApplication.java
@@ -2,7 +2,9 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"ru.practicum.ewm", "ru.practicum.stats"})
 @SpringBootApplication
 public class EwmApplication {
     public static void main(String[] args) {
