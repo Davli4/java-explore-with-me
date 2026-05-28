874a41f13b98308ff5bae859d4e580a436a680bf
Davli4 <vladislauskirey@yandex.by>
2026-05-28 16:42:51 +0300

    fix: 24

--- main-service/src/main/java/ru/practicum/ewm/event/dto/EventFullDto.java
+++ main-service/src/main/java/ru/practicum/ewm/event/dto/EventFullDto.java
@@ -20,6 +20,7 @@ public class EventFullDto {
     private String annotation;
     private CategoryDto category;
     private Long confirmedRequests;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdOn;
     private String description;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
@@ -28,6 +29,7 @@ public class EventFullDto {
     private LocationDto location;
     private Boolean paid;
     private Integer participantLimit;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime publishedOn;
     private Boolean requestModeration;
     private EventState state;
